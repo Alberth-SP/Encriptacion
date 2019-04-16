b62d9c00893fd695f8bd864c3859086cd184654b
Alberth-SP <alberth.cb92@gmail.com>
2019-04-16 08:00:21 +0200

    Agregando generacion de clave

--- Criptografia/cifrado/views.py
+++ Criptografia/cifrado/views.py
@@ -38,24 +38,32 @@ def showKey(_rejilla, _size):
         _string += '\n'
     print(_string)
 
-def generateTabKey(_mess,_size):
-    _count = 0
-    _table = [ [' '] * _size for x in range(_size)]
+def generateTabKey(_mess, _size):
+    _count = 0    
     _points = {}
 
-
-    while _count < (len(_mess)/3):
+    while _count < ((_size*_size)/4):
         _x = int(random() * _size)
-        _y = int(random() * _size)       
+        _y = int(random() * _size)
+        X = _x
+        Y = _y
+        K = str(_x)+'-'+str(_y)
+        _keys = []
 
-        if _table[_x][_y] == ' ':
-            _table[_x][_y] = 'X'
-            _count += 1
+        for i in range(4):
             _k = str(_x)+'-'+str(_y)
-            if not _k in _points.keys():
-                _points[_k] = [_x, _y]
+            _keys.append(_k)
+            _temp = _x
+            _x = _y
+            _y = _size - (_temp + 1) 
 
-    return _table, _points
+        _lista = [ 1 for k in _keys if k in _points.keys() ]
+
+        if not _lista:
+            _points[K] = [X, Y]
+            _count += 1
+
+    return _points
 
 def rotateKey(_table, _size):
     _ntable = {}
@@ -67,98 +75,105 @@ def rotateKey(_table, _size):
             _ntable[_k] = [_x, _y]
     return _ntable
 
-def encrypt(_message, _key, _size):   
-    
+def encrypt(_message, _key, _size):
+
+    _table = [ [''] * _size for x in range(_size)]
+    _index = 0
+    _cipher = ''
     _tmpKey = [[_key[_k][0], _key[_k][1]] for _k in _key]
     _keyOrd = sorted(_tmpKey, key = lambda x: (x[0], x[1]))
-    _newKey = _key
-    _keysRotate = {}
-    _tabsRotate = {}
-    _tmpTab = [ [''] * _size for x in range(_size)] 
 
-    _table = [ [''] * _size for x in range(_size)]  
-    _i = 0
+    for i in range(_size):
+        for j in range(_size):
+            if _index < len(_message):
+                _table[i][j] = _message[_index]
+                _index += 1
+            else:
+                _let = 65
+                _inc = int(random() * (26))
+                _table[i][j] = chr(_let+_inc)    
+    
+    _newKey = _key
+    _keys = {}
+    _ciphers = {}
+    _tmp = ""  
 
     for _turn in range(4):
-        _keysRotate['rotate'+str(_turn)] = _newKey 
-        for _pts in _keyOrd:
-            if _i < len(_message):
-                _table[_pts[0]][_pts[1]] += _message[_i]
-                _tmpTab[_pts[0]][_pts[1]] += _message[_i]
-                _i += 1
-            else:
-                if len(_table[_pts[0]][_pts[1]]) == 0:
-                    _let = 65
-                    _inc = int(random() * (26))
-                    _table[_pts[0]][_pts[1]] += chr(_let+_inc) 
-                    _tmpTab[_pts[0]][_pts[1]] += chr(_let+_inc)       
+        _keys['rotate'+str(_turn)] = _newKey 
+        for _k in _keyOrd:
+            _cipher += _table[_k[0]][_k[1]]
+            _tmp += _table[_k[0]][_k[1]]                 
 
-        _tabsRotate['rotate'+str(_turn)] = _tmpTab    
-        _tmpTab = [ [''] * _size for x in range(_size)]       
+        _ciphers['rotate'+str(_turn)] = _tmp    
+        _tmp = ""
+        _keyOrd = [] 
+        _newKey = rotateKey(_newKey, _size)
+        _tmpKey = [[_newKey[_k][0], _newKey[_k][1]] for _k in _newKey]
+        _keyOrd = sorted(_tmpKey, key = lambda x: (x[0], x[1]))
+   
+    return _cipher, _ciphers, _keys, _table
 
-        _keyOrd = []
+def desencrypt(_message, _key, _size):   
+    
+    _desciphers = {}
+    _keys = {}
+    _descipher = ""
+    _temp = ""
+    _newKey = _key
+    _table = [ [''] * _size for x in range(_size)]
+    _index = 0
+
+    _tmpKey = [[_key[_k][0], _key[_k][1]] for _k in _key]
+    _keyOrd = sorted(_tmpKey, key = lambda x: (x[0], x[1]))
+
+    for _turn in range(4):
+        _keys['rotate'+str(_turn)] = _newKey
+        for _k in _keyOrd:
+            if _index < len(_message):                
+                _table[_k[0]][_k[1]] += _message[_index]           
+                _temp += _message[_index]
+                _index += 1        
+        
+        _desciphers['rotate'+str(_turn)] = _temp
+        _temp = ""
+        _keyOrd = []  
+        print("================================")
+        print(_newKey)
+        print("================================")
         _newKey = rotateKey(_newKey, _size)
         _tmpKey = [[_newKey[_k][0], _newKey[_k][1]] for _k in _newKey]
         _keyOrd = sorted(_tmpKey, key = lambda x: (x[0], x[1]))
 
-    for _x in range(_size):
-        for _y in range(_size):
-            if len(_table[_x][_y]) == 0:
-                _let = 65
-                _inc = int(random() * (26))
-                _table[_x][_y] = chr(_let+_inc)
-
-    return _table, _tabsRotate, _keysRotate
-
-def desencrypt(_tab, _key, _size):
-    _table = _tab
-    _tmpKey = [[_key[_k][0], _key[_k][1]] for _k in _key]
-    _keyOrd = sorted(_tmpKey, key = lambda x: (x[0], x[1]))
-    _newKey = _key
-    _tempTable = [ [''] * _size for x in range(_size)] 
-    _keysRotate = {}
-    _tabsRotate = {}
-    _string = ''
-    _i = 0
-    for _turn in range(4):
-        for _pts in _keyOrd:
-
-            _keysRotate['rotate'+str(_turn)] = _newKey
-            _letters = _table[_pts[0]][_pts[1]]
-
-            if len(_letters) > 1:
-                _table[_pts[0]][_pts[1]] = _letters[1:]
-            _string += _letters[0]
-            _tempTable[_pts[0]][_pts[1]] = _letters[0]
-
-        _tabsRotate['rotate'+str(_turn)] = _tempTable
-        _tempTable = [ [''] * _size for x in range(_size)]
-
-
         
-        _keyOrd = []
-        _newKey = rotateKey(_newKey, _size)
-        _tmpKey = [[_newKey[_k][0], _newKey[_k][1]] for _k in _newKey]
-        _keyOrd = sorted(_tmpKey, key = lambda x: (x[0], x[1]))  
 
-    return _string, _tabsRotate, _keysRotate
+    for _i in range(_size):
+        for _j in range(_size):            
+            _descipher += _table[_i][_j]
+
+
+    return _descipher, _desciphers, _keys, _table
 
 def cipher(request):
     
     _size= request.GET.get('size')
     _message = request.GET.get('message')
-    _message = preprocess(_message)       
+    _message = preprocess(_message)  
+
     _keysRotate = {}
     _tabsRotate = {}
-    table, key = generateTabKey(_message,int(_size))    
-    _tab, _tabsRotate, _keyRotate = encrypt(_message, key, int(_size))    
+
+    key = generateTabKey(_message, int(_size))    
+    cipher, ciphers, keys, table = encrypt(_message, key, int(_size))   
+
     _result = {}
     _result['preproc'] = _message
-    _result['tabs'] = _tabsRotate
-    _result['rotations'] = _keyRotate
-    _result['cipher'] = _tab
+    _result['table'] = table
+    _result['keys'] = keys
+    _result['ciphers'] = ciphers
+    _result['cipher'] = cipher
+   
     writeFile(json.dumps(key), "clave.txt")
-    writeFile(json.dumps(_tab), "salida.txt")
+    writeFile(json.dumps(cipher), "salida.txt")
 
     return HttpResponse(json.dumps(_result))
 
@@ -179,15 +194,24 @@ def readFile(name_file):
 def descipher(request):
     
     _key = readFile("clave.txt")
-    _table = readFile("salida.txt")  
+    _message = readFile("salida.txt")  
     _key = eval(_key)
-    _table = eval(_table)
-    _size = len(_table)
+    _message = eval(_message)
+    _size = request.GET.get('size')
+    print("=======================")
+    print(_message)
+    print(_key)
+    print(_size)
+    print("=======================")
 
-    string, _tabsRotate, _keyRotate = desencrypt(_table, _key, int(_size))
+    descipher, desciphers, keys, table = desencrypt(_message, _key, int(_size))  
+
     _result = {}
-    _result['tabs'] = _tabsRotate
-    _result['descipher'] = string 
+    _result['message'] = _message
+    _result['table'] = table
+    _result['keys'] = keys
+    _result['desciphers'] = desciphers
+    _result['descipher'] = descipher
 
     return HttpResponse(json.dumps(_result)) 
 
