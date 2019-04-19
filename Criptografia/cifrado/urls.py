101a5db29feb64124c15b31d76e6c772bf64e169
Alberth-SP <alberth.cb92@gmail.com>
2019-04-19 08:34:43 +0200

    Agregando botones para cada archivo(clave, mensajes), descarga de archivos

--- Criptografia/cifrado/urls.py
+++ Criptografia/cifrado/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.getPage),
     path('/cipher', views.cipher,name="cipher"),
     path('/descipher', views.descipher,name="descipher"),
+    path('/down_key', views.down_key,name="down_key"),
+    path('/down_cipher', views.down_cipher,name="down_cipher"),
+    path('/down_descipher', views.down_descipher,name="down_descipher"),
 
 ]
--- Criptografia/cifrado/views.py
+++ Criptografia/cifrado/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from random import random 
 from django.http import JsonResponse
+from django.conf import settings
+from django.conf.urls.static import static
 from django.http import HttpResponse
+from django.utils.encoding import smart_str
+
+from wsgiref.util import FileWrapper
 from random import random 
 import json
 import re
+import math
+import os
+
 
 _susti =  {'ñ':'n', 'Ñ':'N'}
 def preprocess(_message):
@@ -37,6 +45,38 @@ def showKey(_rejilla, _size):
                 _string += ' ' + "\t"
         _string += '\n'
     print(_string)
+
+
+def checkKey(_key, _size):
+    _count = 0    
+    _points = {}
+
+    for _k in _key:
+        X = _key[_k][0]
+        Y = _key[_k][1]
+        K = str(X)+'-'+str(Y)   
+        _keys = []
+        _x = X
+        _y = Y
+
+        for i in range(4):
+            _k = str(_x)+'-'+str(_y)                
+            _keys.append(_k)
+            _temp = _x
+            _x = _y
+            _y = _size - (_temp + 1) 
+
+        _lista = [ k for k in _keys if k in _points.keys() ]
+
+        if not _lista:            
+            _points[K] = [X, Y]
+        else:
+            print(_lista)
+            print(_lista)
+            return False
+
+    return True
+
 
 def generateTabKey(_mess, _size):
     _count = 0    
@@ -137,49 +177,82 @@ def desencrypt(_message, _key, _size):
         _desciphers['rotate'+str(_turn)] = _temp
         _temp = ""
         _keyOrd = []  
-        print("================================")
-        print(_newKey)
-        print("================================")
+
         _newKey = rotateKey(_newKey, _size)
         _tmpKey = [[_newKey[_k][0], _newKey[_k][1]] for _k in _newKey]
-        _keyOrd = sorted(_tmpKey, key = lambda x: (x[0], x[1]))
-
-        
+        _keyOrd = sorted(_tmpKey, key = lambda x: (x[0], x[1]))        
 
     for _i in range(_size):
         for _j in range(_size):            
             _descipher += _table[_i][_j]
-
-
+            
     return _descipher, _desciphers, _keys, _table
+
+def down_descipher(request):  
+
+    file_path = os.path.join(settings.MEDIA_ROOT, "descipher.txt")    
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="text/plain")
+            response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(file_path)           
+            return response
+
+def down_cipher(request):  
+
+    file_path = os.path.join(settings.MEDIA_ROOT, "cipher.txt")    
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="text/plain")
+            response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(file_path)           
+            return response
+
+def down_key(request):  
+
+    file_path = os.path.join(settings.MEDIA_ROOT, "key.txt")    
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="text/plain")
+            response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(file_path)           
+            return response
+
 
 def cipher(request):
     
-    _size= request.GET.get('size')
+    _size = request.GET.get('size')
     _message = request.GET.get('message')
-    _message = preprocess(_message)  
+    _message = preprocess(_message) 
+    _result = {}
+    _size = int(_size)
+
+    if(len(_message) == 0):
+        _result['error'] = "MENSAJE NO VALIDO"
+        return HttpResponse(json.dumps(_result))
+    if((_size <= 1) or ((_size%2) != 0) or ((_size * _size) < len(_message))):
+        _result['error'] = "TAMAÑO DE LA CLAVE NO VALIDA"
+        return HttpResponse(json.dumps(_result))
 
     _keysRotate = {}
     _tabsRotate = {}
 
     key = generateTabKey(_message, int(_size))    
     cipher, ciphers, keys, table = encrypt(_message, key, int(_size))   
-
-    _result = {}
+   
     _result['preproc'] = _message
     _result['table'] = table
     _result['keys'] = keys
     _result['ciphers'] = ciphers
     _result['cipher'] = cipher
    
-    writeFile(json.dumps(key), "clave.txt")
-    writeFile(json.dumps(cipher), "salida.txt")
+    writeFile(json.dumps(key), "key.txt")
+    writeFile(json.dumps(cipher), "cipher.txt")
 
     return HttpResponse(json.dumps(_result))
 
 def writeFile(text,name_file):
+
+    file_path = os.path.join(settings.MEDIA_ROOT, name_file)
     if len(text)>0:
-        f = open( name_file, 'w')
+        f = open( file_path, 'w')
         f.write(text)
         f.close()
 
@@ -193,25 +266,39 @@ def readFile(name_file):
 
 def descipher(request):
     
-    _key = readFile("clave.txt")
-    _message = readFile("salida.txt")  
-    _key = eval(_key)
-    _message = eval(_message)
-    _size = request.GET.get('size')
-    print("=======================")
-    print(_message)
-    print(_key)
-    print(_size)
-    print("=======================")
-
-    descipher, desciphers, keys, table = desencrypt(_message, _key, int(_size))  
-
+    _key= request.GET.get('key')
+    _message = request.GET.get('message')
+    _size = 0
     _result = {}
+    try:
+        _key = eval(_key)
+        _message = eval(_message)
+        _size = len(_message)
+        _size = int(math.sqrt(_size)) 
+        print(_key)
+        print(_message)       
+        print(_size)
+    except:
+        _result['error'] = "MENSAJE O CLAVE NO VALIDOS"
+        return HttpResponse(json.dumps(_result))  
+
+    if (_size%2) != 0:
+        _result['error'] = "TAMAÑO DEL MENSAJE NO VALIDO"
+        return HttpResponse(json.dumps(_result))
+
+    if (not checkKey(_key,_size)) or (len(_key)*4 != len(_message)):
+        _result['error'] = "TAMAÑO DE LA CLAVE NO VALIDA"
+        return HttpResponse(json.dumps(_result))
+
+    descipher, desciphers, keys, table = desencrypt(_message, _key, _size)  
+
+    _result['size'] = _size
     _result['message'] = _message
     _result['table'] = table
     _result['keys'] = keys
     _result['desciphers'] = desciphers
     _result['descipher'] = descipher
+    writeFile(json.dumps(descipher), "descipher.txt")
 
     return HttpResponse(json.dumps(_result)) 
 
